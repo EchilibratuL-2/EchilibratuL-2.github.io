39de6940e68d469c81f2509f76077fcfd45f16d6
Duta Gabi <dutagabryel@gmail.com>
2023-11-20 21:18:58 +0200

    update job title with js

--- examples.js
+++ examples.js
@@ -9,13 +9,18 @@ function variableExamples() {
     console.log("Angajat", employed);
     employed = "da";
     console.log("Angajat", employed);
-
+    
     var skills = ["html", "css"];
     skills;
-    console.debug("tipul variabilei skills", typeof skills);
+    console.debug("tipul variabilei skills", typeof skills);       
+}
+
+function updateTitle(title) {
     var job = document.getElementById("job-title");
-    console.warn("job", job);
-    console.info(typeof job);    
+    console.warn("job", job, title);
+    console.info(typeof job);  
+    job.innerHTML = title;  
 }
 
 variableExamples();
+updateTitle("Web Developer @ RWS");
