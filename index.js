187a84cc352389600e80b58410c6b4d2832e0a5e
Duta Gabi <dutagabryel@gmail.com>
2023-12-04 21:00:13 +0200

    add var page for simple code

--- index.js
+++ index.js
@@ -1,51 +1,37 @@
-function show(id) {
-  console.info("show" id)
-
-  var page = document.getElementById(id)
-  console.info("page", page);
-  page.style.display = "block";
-}
+var activePage = "projects";
 
 function hide(id) {
   console.info("hide", id);
   document.getElementById(id).style.display = "none";
 }
 
-function showHome() {
-  hide("skills");
-  hide("projects");
-  hide("languages");
-
-  function show() {
-  var page = document.getElementById("home");
+function show(id) {
+  console.info("show", id);
+  var page = document.getElementById(id);
+  console.debug("show page", page);
   page.style.display = "block";
 }
-show("home");
+
+function showHome() {
+  hide(activePage);
+  show("home");
+  activePage = "home";
 }
 
 function showSkills() {
-  hide("home");
-  hide("projects");
-  hide("languages");
-
-  var page = document.getElementById("skills");
-  page.style.display = "block";
+  hide(activePage);
+  show("skills");
+  activePage = "skills";
 }
 
 function showProjects() {
-  hide("home");
-  hide("skills");
-  hide("languages");
-
-  var page = document.getElementById("projects");
-  page.style.display = "block";
+  hide(activePage);
+  show("projects");
+  activePage = "projects";
 }
 
 function showLanguages() {
-  hide("home");
-  hide("skills");
-  hide("projects");
-
-  var page = document.getElementById("languages");
-  page.style.display = "block";
+  hide(activePage);
+  show("languages");
+  activePage = "languages";
 }
