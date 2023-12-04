a214efc28bb92a041b0b46cbcd9f4e558c6e5a77
Duta Gabi <dutagabryel@gmail.com>
2023-12-04 20:30:59 +0200

    add show function simple

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function show(id) {
+  console.info("show" id)
+
+  var page = document.getElementById(id)
+  console.info("page", page);
+  page.style.display = "block";
+}
+
 function hide(id) {
   console.info("hide", id);
   document.getElementById(id).style.display = "none";
@@ -8,8 +16,11 @@ function showHome() {
   hide("projects");
   hide("languages");
 
+  function show() {
   var page = document.getElementById("home");
   page.style.display = "block";
+}
+show("home");
 }
 
 function showSkills() {
