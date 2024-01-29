5ad3246eca863480ade0d4061b7b0ca50286adf7
Duta Gabi <dutagabryel@gmail.com>
2024-01-29 20:35:40 +0200

    use const and let instead to var

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 // variabile publice
-var activePage = "home";
+let activePage = "home";
 
 // functii publice
 function $(selector) {
-  var el = document.querySelector(selector);
+  const el = document.querySelector(selector);
   // console.info("%o found:", selector, el);
   return el;
 }
 
 function hide(id) {
-  console.info("hide", id);
+  // console.info("hide", id);
   $("#" + id).style.display = "none";
 }
 
 function show(id) {
-  console.info("show", id);
-  var page = document.getElementById(id);
-  console.debug("show page", page);
+  // console.info("show", id);
+  const page = document.getElementById(id);
+  // console.debug("show page", page);
   page.style.display = "block";
 }
 
 function showPage(id) {
-  console.info("show page", id);
-  var prevlink = $("a[data-page=" + activePage + "]");
+  // console.info("show page", id);
+  const prevlink = $("a[data-page=" + activePage + "]");
   prevlink.classList.remove("active");
   hide(activePage);
 
-  var nextLink = $(`a[data-page=${id}]`);
+  const nextLink = $(`a[data-page=${id}]`);
   nextLink.classList.add("active");
   show(id);
   activePage = id;
 }
 
 function initEvents() {
-  var toolbar = $("#top-menu-bar");
+  const toolbar = $("#top-menu-bar");
   toolbar.addEventListener("click", function (e) {
     if (e.target.matches("a")) {
-      var page = e.target.innerHTML.toLowerCase();
-      console.warn("event", page);
+      const page = e.target.dataset.page;
+    // console.warn("click %o", page);
       showPage(page);
     }
   });
 }
 
 function sortSkillByEndorcements(a, b) {
-  console.info("sort", a, b);
+  // console.info("sort", a, b);
   return b.endorcements - a.endorcements;
 }
 
@@ -55,19 +55,19 @@ function sortByName(a, b) {
 function showSkills(skills) {
   // skills.sort(sortSkillByEndorcements);
   skills.sort(sortByName);
-  var ul = $("#skills ul");
+  const ul = $("#skills ul");
 
-  var text = skills.map(function (skill) {
-    var cls = "";
+  const text = skills.map(function (skill) {
+    let cls = "";
     if (skill.favorite == true) {
-      console.warn("f", skill);
+      // console.warn("f", skill);
       cls = "favorite";
     }
 
-    console.info("inside %o map", skill.favorite, cls);
+    // console.info("inside %o map", skill.favorite, cls);
     return `<li class="${cls}">${skill.name} <span> - ${skill.endorcements}</span></li>`;
   });
-  console.warn(text);
+  // console.warn(text);
 
   ul.innerHTML = text.join("");
 }
